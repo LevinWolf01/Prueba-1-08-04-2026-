087deb2c1d8186c04b077f67d593be237e1efd33
LevinWolf01 <danie.a.lopez2020@gmail.com>
2026-04-08 11:10:11 -0500

    chore: Configuración inicial del proyecto CodeRider Flow

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import { version } from "react";
+
+export function saludar(nombre){
+    return "hola " + nombre + ", bienvenido a la aplicación de CodeRider!";
+}
+
+export function despedir(nombre){
+    return "Hata pronto, " + nombre + " ¡CodeRider!";
+}
+
+export function estadoSistema(){
+    return {
+        estado: "activo",
+        version: "1.0.0",
+        message: "Sistema CodeRider funcionando Correctamente"
+    };
+}
---
+++ test/app.test.js
@@ -0,0 +1,34 @@
+import {saludar, despedir, estadoSistema} from "../src/app.js"
+
+function ejecutarPruebas(){
+    let pasadas     = 0;
+    let fallidas    = 0;
+
+    // TESTEO FUNCIÓN SALUDAR
+    const R1        = saludar("Rider");
+    if (R1.includes("Rider")){
+        console.log ("Test 1 Pasado 👍: Funcion Saludar en CodeRider es correcta!");
+        pasadas++;
+    } 
+
+    else {
+        console.log ("Test 1 Fallido 😓", R1);
+        fallidas++;
+    }
+
+    // FUNCION ESTADO SISTEMA
+    const actualEstado = estadoSistema()
+    if (actualEstado.estado === "activo"){
+        console.log ("Test 2: Pasado👌: 'estadoSistema()' funciona en CodeRider");
+        pasadas++;
+    }
+    else {
+        console.log ("Test 2: Fallido👎: ", estado, "en 'codeRider");
+        fallidas++;
+    }
+
+    console.log ("\nResultados: " + pasadas + " pasadas; ", + fallidas + " fallidas");
+    if (fallidas > 0) {process.exit(1)}
+}
+
+ejecutarPruebas();
